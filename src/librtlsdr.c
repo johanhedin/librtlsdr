fc1e1327ac69af94288fac26ef7d7d4b4e57fe4f
Johan Hedin <johan.o.hedin@gmail.com>
2024-01-11 16:07:14 +0100

    Do not use libusb zero copy at all

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -3880,7 +3880,7 @@ static int _rtlsdr_alloc_async_buffers(rtlsdr_dev_t *dev)
 	dev->xfer_buf = malloc(dev->xfer_buf_num * sizeof(unsigned char *));
 	memset(dev->xfer_buf, 0, dev->xfer_buf_num * sizeof(unsigned char *));
 
-#if defined (__linux__) && LIBUSB_API_VERSION >= 0x01000105
+#if defined USE_ZEROCOPY && (__linux__) && LIBUSB_API_VERSION >= 0x01000105
 	fprintf(stderr, "Allocating %d zero-copy buffers\n", dev->xfer_buf_num);
 
 	dev->use_zerocopy = 1;
